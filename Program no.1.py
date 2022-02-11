6cdd8f8e7d120b6fc802a052cf9f038a29ec2451
Lemonade-up <wmaglangit1118@gmail.com>
2022-02-12 03:18:16 +0800

    Changed height of text from 10mm into 7mm

--- Program no.1.py
+++ Program no.1.py
@@ -31,59 +31,59 @@ resume.cell(100, 10, "Full Stack Developer", ln = True)
 
 # Personal info
 resume.set_font('Times', 'B', 18)
-resume.cell(0,10 , "Personal Information", ln = True)
+resume.cell(0,7 , "Personal Information", ln = True)
 resume.set_font('Helvetica', '', 12)
-resume.cell(50,10, 'Sex: ' + str(data["Personal Info"][0]["Sex"]), ln = True) 
-resume.cell(50,10, 'Age: ' + str(data["Personal Info"][0]["Age"]), ln = True)
-resume.cell(50,10, 'Address: ' + str(data["Personal Info"][0]["Address"]), ln = True)
-resume.cell(50,10, 'Contact#: ' + str(data["Personal Info"][0]["Contact Number"]), ln = True)
-resume.cell(50,10, 'Email Address: ' + str(data["Personal Info"][0]["Email Address"]), ln = True)
-resume.cell(50,10, 'Github: ' + str(data["Personal Info"][0]["Github Account"]), ln = True)
+resume.cell(50,7, 'Sex: ' + str(data["Personal Info"][0]["Sex"]), ln = True) 
+resume.cell(50,7, 'Age: ' + str(data["Personal Info"][0]["Age"]), ln = True)
+resume.cell(50,7, 'Address: ' + str(data["Personal Info"][0]["Address"]), ln = True)
+resume.cell(50,7, 'Contact#: ' + str(data["Personal Info"][0]["Contact Number"]), ln = True)
+resume.cell(50,7, 'Email Address: ' + str(data["Personal Info"][0]["Email Address"]), ln = True)
+resume.cell(50,7, 'Github: ' + str(data["Personal Info"][0]["Github Account"]), ln = True)
 
 # Summary
 resume.set_font('Times', 'B', 18)
-resume.cell(0,10 , "Professional Summary", ln = True)
+resume.cell(0,7 , "Professional Summary", ln = True)
 resume.set_font('Helvetica', '', 12)
-resume.cell(50,10, str(data["Professional Summary"]), ln = True) 
+resume.cell(50,7, str(data["Professional Summary"]), ln = True) 
 
 # Experience
 resume.set_font('Times', 'B', 18)
-resume.cell(0,10 , "Work Experience", ln = True)
+resume.cell(0,7 , "Work Experience", ln = True)
 resume.set_font('Helvetica', '', 12)
-resume.cell(50,10, 'Former Work: ' + str(data["Work Experience"]["Former Work"]), ln = True) 
-resume.cell(50,10, 'Company: ' + str(data["Work Experience"]["Company"]), ln = True)
-resume.cell(50,10, 'Work Description: ' + str(data["Work Experience"]["Work Description"]), ln = True)
+resume.cell(50,7, 'Former Work: ' + str(data["Work Experience"]["Former Work"]), ln = True) 
+resume.cell(50,7, 'Company: ' + str(data["Work Experience"]["Company"]), ln = True)
+resume.cell(50,7, 'Work Description: ' + str(data["Work Experience"]["Work Description"]), ln = True)
 
 
 # Education
 resume.set_font('Times', 'B', 18)
-resume.cell(0,10 , "Educational Background", ln = True)
+resume.cell(0,7 , "Educational Background", ln = True)
 resume.set_font('Helvetica', '', 12)
-resume.cell(50,10, 'Course: ' + str(data["Educational Background"][0]["Course"]), ln = True) 
-resume.cell(50,10, 'University: ' + str(data["Educational Background"][0]["University"]), ln = True)
-resume.cell(50,10, 'Course: ' + str(data["Educational Background"][1]["Course"]), ln = True)
-resume.cell(50,10, 'University: ' + str(data["Educational Background"][1]["University"]), ln = True)
+resume.cell(50,7, 'Course: ' + str(data["Educational Background"][0]["Course"]), ln = True) 
+resume.cell(50,7, 'University: ' + str(data["Educational Background"][0]["University"]), ln = True)
+resume.cell(50,7, 'Course: ' + str(data["Educational Background"][1]["Course"]), ln = True)
+resume.cell(50,7, 'University: ' + str(data["Educational Background"][1]["University"]), ln = True)
 
 
 # Projects
 resume.set_font('Times', 'B', 18)
-resume.cell(0,10 , "Projects", ln = True)
+resume.cell(0,7 , "Projects", ln = True)
 resume.set_font('Helvetica', '', 12)
-resume.cell(50,10, 'Project Name: ' + str(data["Projects"]["Project Name"]), ln = True) 
-resume.cell(50,10, 'Position: ' + str(data["Projects"]["Position"]), ln = True)
-resume.cell(50,10, 'Project Description: ' + str(data["Projects"]["Project Description"]), ln = True)
+resume.cell(50,7, 'Project Name: ' + str(data["Projects"]["Project Name"]), ln = True) 
+resume.cell(50,7, 'Position: ' + str(data["Projects"]["Position"]), ln = True)
+resume.cell(50,7, 'Project Description: ' + str(data["Projects"]["Project Description"]), ln = True)
 
 
 # Skills
 resume.set_font('Times', 'B', 18)
-resume.cell(0,10 , "Skills", ln = True)
+resume.cell(0,7 , "Skills", ln = True)
 resume.set_font('Helvetica', '', 12)
-resume.cell(50,10, str(data["Skills"][0]), ln = False) 
-resume.cell(50,10, str(data["Skills"][1]), ln = True)
-resume.cell(50,10, str(data["Skills"][2]), ln = False)
-resume.cell(50,10, str(data["Skills"][3]), ln = True)
-resume.cell(50,10, str(data["Skills"][4]), ln = False)
-resume.cell(50,10, str(data["Skills"][5]), ln = True)
+resume.cell(50,7, str(data["Skills"][0]), ln = False) 
+resume.cell(50,7, str(data["Skills"][1]), ln = True)
+resume.cell(50,7, str(data["Skills"][2]), ln = False)
+resume.cell(50,7, str(data["Skills"][3]), ln = True)
+resume.cell(50,7, str(data["Skills"][4]), ln = False)
+resume.cell(50,7, str(data["Skills"][5]), ln = True)
 
 #Output
 resume.output('MAGLANGIT,Wanjin P..pdf')
